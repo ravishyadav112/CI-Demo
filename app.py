bed72bf07da112d7f4bd6952c314838779d9f100
ravishyadav112 <ravishkumar1242@gmail.com>
2025-08-31 15:30:37 +0530

    Adding yaml file

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 def add(a, b):
     return a + b
 
-def sub(a , b ):
-    return a - b 
 
-def mul(a , b):
-    return a *  b 
+def sub(a, b):
+    return a - b
 
-print(add(2,3))
-print(sub(8,7))
-print(mul(8,4))
+
+def mul(a, b):
+    return a * b
+
+
+print(add(2, 3))
+print(sub(8, 7))
+print(mul(8, 4))
